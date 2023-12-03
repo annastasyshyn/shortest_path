7bb8edba093ccd5abac948ea39239312bc504d98
annastasyshyn <annastasyshynn@gmail.com>
2023-12-03 23:29:13 +0200

    dijkstra + function for generating test csv files

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 """Project"""
 import math
+import heapq
 
 
 def read(file_path: str) -> list[list[int]]:
     """Read data from csv file"""
     with open(file_path, 'r', encoding='utf-8') as file:
         data = file.read()
-    matrix = [[int(i) for i in j.split(',')] for j in data.strip().split('\n')]
-    return matrix
+    return [[int(i) for i in j.split(',')] for j in data.strip().split('\n')]
 
-def matrix_to_graph(matrix: list[list[int]]) -> dict:
+def matrix_to_graph(matr: list[list[int]]) -> dict:
     """Turn matrix into a graph"""
     graph = {}
-    rows, cols = len(matrix), len(matrix[0])
+    rows, cols = len(matr), len(matr[0])
     for i in range(rows):
         for j in range(cols):
             vertex = (i, j)
@@ -27,7 +27,7 @@ def matrix_to_graph(matrix: list[list[int]]) -> dict:
                 graph[vertex].append((i, j + 1))
     return graph
 
-def distance(point_a: tuple[int], point_b: tuple[int], step: int, matrix: list[list[int]]):
+def calculate_distance(point_a: tuple[int], point_b: tuple[int], step: int, matrix: list[list[int]]):
     """-"""
     height_difference = abs(matrix[point_a[0]][point_a[1]] - matrix[point_b[0]][point_b[1]])
     physical_distance = math.sqrt((point_a[0] \
@@ -35,6 +35,38 @@ def distance(point_a: tuple[int], point_b: tuple[int], step: int, matrix: list[l
     edge_weight = math.sqrt(height_difference ** 2 + physical_distance ** 2)
     return edge_weight
 
+def dijkstra(graph, start, end, matr, step):
+    """-"""
+    distances = {vertex: float('infinity') for vertex in graph}
+    distances[start] = 0
+    priority_queue = [(0, start)]
 
-# print(matrix_to_graph(read('/home/Anya/UCU/pb/discrete_maths_project/example.csv')))
-# print(distance((0,0), (2,2), 1, read('/home/Anya/UCU/pb/discrete_maths_project/example.csv')))
+    while priority_queue:
+        current_distance, current_vertex = heapq.heappop(priority_queue)
+
+        if current_distance > distances[current_vertex]:
+            continue
+
+        for neighbor in graph[current_vertex]:
+            weight = calculate_distance(current_vertex, neighbor, step, matr)
+            distance = distances[current_vertex] + weight
+
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                heapq.heappush(priority_queue, (distance, neighbor))
+
+    path, current_vertex = [], end
+    while current_vertex != start:
+        path.insert(0, current_vertex)
+        current_vertex = min(graph[current_vertex], key=lambda x: distances[x])
+
+    path.insert(0, start)
+    return path
+
+matrix = read('/home/Anya/UCU/pb/discrete_maths_project/matr1.csv')
+gr = matrix_to_graph(matrix)
+a = (0,0)
+b = (999,999)
+STEP = 1
+path = dijkstra(gr, a, b, matrix, STEP)
+print(path)
