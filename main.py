20d4dea6d523655c37637a46becb666f02f419cf
anya <89141571+annastasyshyn@users.noreply.github.com>
2023-12-06 21:23:20 +0200

    updated optimized version of the algorithm

--- main.py
+++ main.py
@@ -1,74 +1,86 @@
-"""Project"""
-import math
+"""Finding the shortest path between two points on the on the surface"""
 import heapq
+import numpy as np
+import pandas as pd
+from numba import jit, int64, float64
 
 
-def read(file_path: str) -> list[list[int]]:
-    """Read data from csv file"""
-    with open(file_path, 'r', encoding='utf-8') as file:
-        data = file.read()
-    return [[int(i) for i in j.split(',')] for j in data.strip().split('\n')]
+def read_matrix_from_csv(file_path):
+    """
+    Read matrix from csv
+    """
+    return pd.read_csv(file_path, header=None).to_numpy()
 
-def matrix_to_graph(matr: list[list[int]]) -> dict:
-    """Turn matrix into a graph"""
-    graph = {}
-    rows, cols = len(matr), len(matr[0])
-    for i in range(rows):
-        for j in range(cols):
-            vertex = (i, j)
-            graph[vertex] = []
-            if i > 0:
-                graph[vertex].append((i - 1, j))
-            if i < rows - 1:
-                graph[vertex].append((i + 1, j))
-            if j > 0:
-                graph[vertex].append((i, j - 1))
-            if j < cols - 1:
-                graph[vertex].append((i, j + 1))
-    return graph
+@jit(nopython=True)
+def calculate_distance(height_diff, step):
+    """
+    Calculate the physical distance between two points.
+    (The square root of the sum of the squares of the 
+    height difference and the number step)
+    """
+    return np.sqrt(height_diff**2 + step**2)
 
-def calculate_distance(point_a: tuple[int], point_b: tuple[int], \
-step: int, matrix: list[list[int]]):
-    """Calculate distance"""
-    height_difference = abs(matrix[point_a[0]][point_a[1]] - matrix[point_b[0]][point_b[1]])
-    physical_distance = math.sqrt((point_a[0] \
-- point_b[0]) ** 2 + (point_a[1] - point_b[1]) ** 2) * step
-    edge_weight = math.sqrt(height_difference ** 2 + physical_distance ** 2)
-    return edge_weight
+@jit(nopython=True)
+def heuristic(node, end, step):
+    """
+    Heuristic function
+    """
+    return np.sqrt((node[0] - end[0])**2 + (node[1] - end[1])**2) * step
 
-def dijkstra(graph: dict, start: tuple, end: tuple, \
-matr: list[list[int]], step: int) -> list[tuple[int]]:
-    """Dijkstra algorithm"""
-    distances = {vertex: float('infinity') for vertex in graph}
-    distances[start] = 0
-    priority_queue = [(0, start)]
+@jit(nopython=True)
+def get_neighbors(x, y, rows, cols):
+    """
+    Get neighbours of a node
+    """
+    neighbors = []
+    for dx in [-1, 0, 1]:
+        for dy in [-1, 0, 1]:
+            if dx == 0 and dy == 0:
+                continue
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < rows and 0 <= ny < cols:
+                neighbors.append((nx, ny))
+    return neighbors
 
-    while priority_queue:
-        current_distance, current_vertex = heapq.heappop(priority_queue)
+@jit(nopython=True)
+def shortest_path(M, step, A, B):
+    """
+    The function for finding the shortest path (A-star)
+    """
+    rows, cols = np.shape(M)
 
-        if current_distance > distances[current_vertex]:
-            continue
+    start = A
+    end = B
 
-        for neighbor in graph[current_vertex]:
-            weight = calculate_distance(current_vertex, neighbor, step, matr)
-            distance = distances[current_vertex] + weight
+    distances = np.full((rows, cols), np.inf, dtype=float64)
+    distances[start] = 0.0
 
-            if distance < distances[neighbor]:
-                distances[neighbor] = distance
-                heapq.heappush(priority_queue, (distance, neighbor))
+    previous_vertices = np.full((rows, cols, 2), -1, dtype=int64)
 
-    path, current_vertex = [], end
-    while current_vertex != start:
-        path.insert(0, current_vertex)
-        current_vertex = min(graph[current_vertex], key=lambda x: distances[x])
+    vertices = [(0.0 + heuristic(start, end, step), start[0], start[1])]
 
-    path.insert(0, start)
-    return path
+    while vertices:
+        _, current_row, current_col = heapq.heappop(vertices)
 
-# matrix = read('/home/Anya/UCU/pb/discrete_maths_project/matr1.csv')
-# gr = matrix_to_graph(matrix)
-# a = (0,0)
-# b = (999,999)
-# STEP = 1
-# path = dijkstra(gr, a, b, matrix, STEP)
-# print(path)
+        for neighbor_row, neighbor_col in get_neighbors(current_row, current_col, rows, cols):
+            height_diff = abs(M[current_row, current_col] - M[neighbor_row, neighbor_col])
+            new_distance = distances[current_row, current_col] \
++ calculate_distance(height_diff, step)
+
+            if new_distance < distances[neighbor_row, neighbor_col]:
+                distances[neighbor_row, neighbor_col] = new_distance
+                previous_vertices[neighbor_row, neighbor_col, 0] = current_row
+                previous_vertices[neighbor_row, neighbor_col, 1] = current_col
+                heapq.heappush(vertices, (new_distance + \
+heuristic((neighbor_row, neighbor_col), end, step), neighbor_row, neighbor_col))
+
+    path = []
+    current_vertex = end
+    while not np.all(previous_vertices[current_vertex[0], \
+current_vertex[1]] == -1):
+        path.append((current_vertex[0], current_vertex[1]))
+        current_vertex = (previous_vertices[current_vertex[0], \
+current_vertex[1], 0], previous_vertices[current_vertex[0], current_vertex[1], 1])
+
+    path.append((start[0], start[1]))
+    return path[::-1]
